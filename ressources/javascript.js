55ba54a8639c56ca7609c50898f9ac0edc2fdf4c
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-09-27 12:07:31 +0200

    organiz

--- ressources/javascript.js
+++ ressources/javascript.js
@@ -1,1 +1,24 @@
+window.addEventListener("load",(event)=>{
+
+    fonctionCache()
     
+   })
+  
+  
+  document.querySelector('.fas').addEventListener("click",(event)=>{
+  
+    fonctionCache()
+    
+   })
+  
+  
+  function fonctionCache() {
+    let menu = document.getElementById("menu");
+  
+    if (menu.style.display === "none") {
+      menu.style.display = "flex";
+    } else {
+      menu.style.display = "none";
+    }
+  }
+      
