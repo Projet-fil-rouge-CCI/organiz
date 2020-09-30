226250e94d7889efc971691a77ae8c75980455e4
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-09-30 15:48:55 +0200

    organiz

---
+++ controllers/factureDetails.php
@@ -0,0 +1,20 @@
+<?php
+
+include '../models/BDDfactureDetails.php';
+
+
+
+if(!empty($_POST)){
+
+
+ajout(trim($_POST['titre']),($_POST['objet']),($_POST['quantite']),($_POST['prix']),($_POST['tva']));
+
+
+}
+
+
+
+require '../views/factureDetails.phtml';
+
+
+?>
