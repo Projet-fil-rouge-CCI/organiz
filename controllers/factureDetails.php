4b378bb4251d874bc388e0b5e3991d0f663225e5
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-10-01 11:54:59 +0200

    organiz

--- controllers/factureDetails.php
+++ controllers/factureDetails.php
@@ -7,7 +7,7 @@ include '../models/BDDfactureDetails.php';
 if(!empty($_POST)){
 
 
-ajout(trim($_POST['titre']),($_POST['objet']),($_POST['quantite']),($_POST['prix']),($_POST['tva']));
+ajout(trim($_POST['titre']),($_POST['quantite']),($_POST['prix']),($_POST['tva']));
 
 
 }
---
+++ models/BDDfactureDetails.php
@@ -0,0 +1,45 @@
+<?php
+
+
+session_start();
+if(!array_key_exists('userID',$_SESSION)){
+	header('location:../controllers/connexion.php');
+	exit;
+}
+
+
+
+define('DATABASE_DSN','mysql:host=localhost;dbname=organiz;charset=utf8');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+
+
+
+function ajout($titre,$quantite,$prix,$tva){
+
+	$dbh = new PDO
+	(
+		DATABASE_DSN,
+		DATABASE_USERNAME,
+		DATABASE_PASSWORD,
+		[
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+		]
+	);
+
+$query='INSERT INTO facture_details (quantite,prix_unitaire,nom,TVA) values (:quantite,:prix,:nom,:TVA)';
+$sth=$dbh ->prepare($query);
+$sth->bindValue(':quantite',$quantite,PDO::PARAM_INT);
+$sth->bindValue(':prix',$prix,PDO::PARAM_STR);
+$sth->bindValue(':nom',$titre,PDO::PARAM_STR);
+$sth->bindValue(':TVA',$tva,PDO::PARAM_STR);
+
+$sth->execute();
+
+}
+
+
+
+
+?>
