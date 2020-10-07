2a72696aacc746f15f37a3b183542c144c3f5eec
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-10-07 10:31:22 +0200

    organiz

--- controllers/vueFacture.php
+++ controllers/vueFacture.php
@@ -5,10 +5,13 @@ require ('../models/BBDvueFacture.php');
 $vueFacture=vueFacture(($_GET['id_facture']));
 $vueDetailsFacture=vueDetailsFacture(($_GET['id_facture']));
 $nameSoc=nomSociete(($_SESSION['idsoc']));
+$add=0;
 
 /*var_dump($vueFacture);
-var_dump($vueDetailsFacture);
+
 var_dump($nameSoc);*/
+
+var_dump($vueDetailsFacture);
 
 
 include ('../views/vueFacture.phtml');
